334019978473c7177e483faf0a7b1853e41e0b5d
TimsTittus <timstittus1@gmail.com>
2026-06-01 22:15:58 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@Saints/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
